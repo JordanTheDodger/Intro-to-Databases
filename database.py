c39c3b9aca248c91a4edd1a425f5e0cb60d687d8
JordanTheDodger <54824287+JordanTheDodger@users.noreply.github.com>
2019-11-25 15:49:41 -0500

    Update database.py

--- database.py
+++ database.py
@@ -1,14 +1,24 @@
 import psycopg2
 from psycopg2 import pool
 
+connection_pool = pool.SimpleConnectionPool(1, 1, database="learning", user="postgres", password="1033",
+                                            host="localhost")
 
-class ConnectionPool():
+"""getting one connection from connection pool and keep track of connection that it represents"""
+
+
+class ConnectionFromPool():
     def __int__(self):
-        self.connection_pool = pool.SimpleConnectionPool(1, 1, database="learning", user="postgres", password="1033",
-                                                         host="localhost")
+        self.connection = None
 
-    def __enter__(self):
-        return self.connection_pool.getconn()
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        pass
+"""at the enter of with statement enter is called and at the return exit is called"""
+
+
+def __enter__(self):
+    self.connection = connection_pool.getconn()
+    return self.connection
+
+
+def __exit__(self, exc_type, exc_val, exc_tb):
+    connection_pool.putconn(self.connection)
